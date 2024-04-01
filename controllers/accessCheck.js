01fcc43e53dc46198e9d427ada480ac22a55d35a
bhanu2004 <bhanuprksh05@gmail.com>
2024-04-01 11:44:26 +0530

    access check res updated

--- controllers/accessCheck.js
+++ controllers/accessCheck.js
@@ -4,7 +4,7 @@ const accessCheck = (req,res,obj,idName)=>{
             if(!req.body[idName]) reject(`${idName} is missing`);
             const data = await obj.findAll({where:{id:req.body[idName]}})
             if(data[0].dataValues.coordinatorId === req.user[0].dataValues.id){
-                 resolve("success");
+                 resolve("Authorization success");
             } else{
                 reject("Unauthorized access!")
             }
